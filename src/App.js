c10a3fa9daa5766ce02a674c1484b918b3456ef7
Vishal Joshi <vishalj026@gmail.com>
2022-05-29 22:53:36 +0530

    Added navigation bar and basic setup for routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import CategoryList from './components/category-list/category-list.component';
+import {Routes, Route} from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 function App() {
+  const Shop = () => {
+    return (
+      <h2>Shop Component</h2>
+    );
+  }
   return (
-    <CategoryList />
+    // <CategoryList />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path='shop' element={<Shop />}></Route>
+      </Route>
+    </Routes>
+    
   );
 }
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,9 @@
+import CategoryList from '../../components/category-list/category-list.component'
+
+function Home() {
+  return (
+    <CategoryList />
+  );
+}
+
+export default Home;
